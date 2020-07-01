0399b8d130c23a1d5123d49df142b1ee229c8990
RiadHossain <riadhossain7464@gmail.com>
2020-07-01 12:49:58 +0600

    New Update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 
-$(document).ready(()=>{
-    $('.menu-btn').click(()=>{
-        $('.nav').toggleClass('nav-expand')
+// $(document).ready(()=>{
+//     $('.menu-btn').click(()=>{
+//         $('.nav').toggleClass('nav-expand')
+//     })
+// })
+
+window.addEventListener('load',()=>{
+    const menu_btn = document.querySelector('.menu-btn')
+    const nav = document.querySelector('.nav')
+
+    menu_btn.addEventListener('click',()=>{
+        nav.classList.toggle('nav-expand')
+        menu_btn.classList.toggle('menu-open')
     })
+
+
+
+
+
 })
