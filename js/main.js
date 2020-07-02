c6bd4438630f1a7eb9114ffd9366b0ac4b00ed0d
RiadHossain <riadhossain7464@gmail.com>
2020-07-02 18:30:14 +0600

    New Design

--- js/main.js
+++ js/main.js
@@ -5,27 +5,78 @@
 //     })
 // })
 
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
+    const body = document.querySelector('body')
     const menu_btn = document.querySelector('.menu-btn')
     const nav = document.querySelector('.nav')
     const make_req = document.querySelectorAll('button')
+    const sub_link = document.querySelectorAll('.sub-link')
     const req_form_container = document.querySelector('.req_form_container')
-    const req_back = document.querySelectorAll('.req_back')
-    menu_btn.addEventListener('click',()=>{
+
+    // const req_form_content = document.querySelector('.req_form_container')
+
+    menu_btn.addEventListener('click', () => {
         nav.classList.toggle('nav-expand')
         menu_btn.classList.toggle('menu-open')
     })
 
-    make_req.forEach( btn => {
-        btn.addEventListener('click',function(e){
+    make_req.forEach(btn => requestService(btn))
+    sub_link.forEach(btn => requestService(btn))
+
+
+    function requestService(btn) {
+        // let req_form_container = document.createElement('div')
+        // req_form_container.classList.add('req_form_container')
+        // body.appendChild(req_form_container)
+        btn.addEventListener('click', function (e) {
+
             req_form_container.style.display = 'block'
+            req_form_container.innerHTML = `
+                <button class="req_back">X</button>
+                <h2 style="text-align: center;">Fill the form to make a request for the service</h2>
+                <form class="req-form" action="" method="POST">
+                    <div class="half_width field req-f"><input class="input" type="text" name="name" id="Name"
+                            placeholder="Name"></div>
+                    <div class="half_width field req-f"><input class="input" type="text" name="phone" id="Phone"
+                            placeholder="Phone"></div>
+                    <div class="half_width field req-f"><input class="input" type="text" name="email" id="Email"
+                            placeholder="Email"></div>
+                    <div class="half_width field req-f"><input class="input" type="text" name="organization" id="organization"
+                            placeholder="Organisation"></div>
+                    <div class="full_width field req-f"><input class="input" type="text" name="subject" id="company"
+                            placeholder="Subject"></div>
+                    <div class="full_width field req-f"><textarea name="message" id="about_product"
+                            placeholder="Write a message ..."></textarea></div>
+                    <div class="full_width field"><button class="submit_btn">Submut</button>
+                    </div>
+                </form>
+                `
+            let req_back = document.querySelectorAll('.req_back')
+            req_back.forEach(btn => {
+                btn.addEventListener('click', (e) => {
+                    req_form_container.style.display = ' none'
+                    req_form_container.innerHTML = ''
+                })
+            })
         })
-    })
-    req_back.forEach(btn=>{
-        btn.addEventListener('click',(e)=>{
-            e.target.parentElement.style.display = ' none'
-        })
-    })
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
