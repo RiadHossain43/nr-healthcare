ffffbfcb3d0c1840962b692625e22f542f9879d7
RiadHossain <riadhossain7464@gmail.com>
2020-07-08 16:36:16 +0600

    New Anim

--- js/indexanim.js
+++ js/indexanim.js
@@ -9,4 +9,6 @@ window.addEventListener('load', () => {
         .from('.title p', { y: '25%', opacity: 0, duration: .5 })
         .from('.title a', { y: '25%', opacity: 0, duration: .5 })
         .from('.brand-name a',{y:'120%',rotation:720})
+        .from('.style-icon-1:nth-of-type(1)',{x:'-40%',opacity:1},2)
+        .from('.style-icon-1:nth-of-type(2)',{x:'40%',opacity:1},2)
 })
