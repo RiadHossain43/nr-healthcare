7188a1376a7c0be3d443fb5c0b95c0c0d3c4758a
RiadHossain <riadhossain7464@gmail.com>
2020-07-08 16:42:37 +0600

    New Anim

--- js/serviceAnim.js
+++ js/serviceAnim.js
@@ -1,8 +1,9 @@
+window.addEventListener('load', () => {
+    const timeline = gsap.timeline({ defaults: { duration: .7 } })
 
-const timeline = gsap.timeline({ defaults: { duration: .7 } })
-
-timeline
-    .from('.service-detail h1', { y: '-100%' })
-    .from('.service-detail p', { y: '15%', opacity: 0 })
-    .from('.style-icon-1:nth-of-type(1)', { x: '-40%', opacity: 1 }, 2)
-    .from('.style-icon-1:nth-of-type(2)', { x: '40%', opacity: 1 }, 2)
+    timeline
+        .from('.service-detail h1', { y: '-100%' })
+        .from('.service-detail p', { y: '15%', opacity: 0 })
+        .from('.style-icon-1:nth-of-type(1)', { x: '-40%', opacity: 1 }, 2)
+        .from('.style-icon-1:nth-of-type(2)', { x: '40%', opacity: 1 }, 2)
+})
