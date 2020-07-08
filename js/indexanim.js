e5610d193af86fee18fa4aab96c3f84a1452319f
RiadHossain <riadhossain7464@gmail.com>
2020-07-08 16:03:22 +0600

    :

--- js/indexanim.js
+++ js/indexanim.js
@@ -1,11 +1,11 @@
-// gsap.from('header',{duration:1, y:'-100%',ease:'bounce'})
-// gsap.from('.link',{duration:1,opacity:0,delay:1,stagger:.2})
 
-const timeline = gsap.timeline({defaults:{duration:.7}})
-// const menu_btn = document.querySelector('.menu-btn')
-timeline
-.from('header',{y:'-100%',ease:'bounce'})
-.from('.title h1',{x:'-5%',opacity:0,ease:'elastic'})
-.from('.title h2',{y:'25%',opacity:0,duration:.5})
-.from('.title p',{y:'25%',opacity:0,duration:.5})
-.from('.title a',{y:'25%',opacity:0,duration:.5})
+window.addEventListener('load', () => {
+    const timeline = gsap.timeline({ defaults: { duration: .7 } })
+
+    timeline
+        .from('header', { y: '-100%', ease: 'bounce' })
+        .from('.title h1', { x: '-5%', opacity: 0, ease: 'elastic' })
+        .from('.title h2', { y: '25%', opacity: 0, duration: .5 })
+        .from('.title p', { y: '25%', opacity: 0, duration: .5 })
+        .from('.title a', { y: '25%', opacity: 0, duration: .5 })
+})
