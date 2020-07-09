ccdd6881eb1e1ca9a6a0b82b19df074c859d19ae
RiadHossain <riadhossain7464@gmail.com>
2020-07-09 11:27:18 +0600

    NEw UPdate

--- js/indexanim.js
+++ js/indexanim.js
@@ -1,6 +1,4 @@
-window.addEventListener('load', () => {
-
-    const timeline = gsap.timeline({ defaults: { duration: .7 } })
+const timeline = gsap.timeline({ defaults: { duration: .7 } })
 
     //  timeline
     //      .from('header', { y: '-100%', ease: 'bounce' })
@@ -21,4 +19,3 @@ window.addEventListener('load', () => {
         .fromTo('.brand-name a', { y: '120%', rotation: 720 }, { y: '0%', rotation: 0 })
         .fromTo('.style-icon-1:nth-of-type(1)', { x: '-40%', opacity: 1 }, 2)
         .fromTo('.style-icon-1:nth-of-type(2)', { x: '40%', opacity: 1 }, 2)
-})
