2da33fa7f463a6808b438af2d16d646a09393bfd
RiadHossain <riadhossain7464@gmail.com>
2020-07-15 22:07:37 +0600

    New Update

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ const data = [
     {
         _study: "NHS GMSS case study",
         _intro: `
-        The global pandemic caused by COVID-19 has taught us many things about the way we live and work. For NHS Greater Manchester Shared Services, it put into practice months of hard work and demonstrated the value of ISO 27001 certification.
+        The global pandemic caused by COVID-19 has taught us many things about the way we live and work. For NHS Greater Manchester Shared Services, it put into practice months of hard work and demonstrated the value of ISO....
         `,
         _detail: `The global pandemic caused by COVID-19 has taught us many things about the way we live and work. For NHS Greater Manchester Shared Services, it put into practice months of hard work and demonstrated the value of ISO 27001 certification.<br><br>
         Eighteen months ago, Phil Scott, IT Security Manager at NHS Greater Manchester Shared Services (GMSS), and his team made the decision to work towards ISO 27001 certification with leading certification provider, BM TRADA. The certification was awarded in mid-June, by which time the benefits of the process were already clear to see.<br><br>
@@ -28,9 +28,61 @@ const data = [
         `
     },
     {
-        _study: "NHS GMSS case study",
-        _intro: `kello World`,
-        _detail: `bello World`
+        _study: "KATLAS Technology Ltd ",
+        _intro: `Katlas Technology has developed MAI Health, a clinical system comprising platforms and a portal that enables fast-track, remote monitoring of healthcare.<br><br> MAI Health integrates with medical devices and an AI engine (NEOS) to provide a Proximity Care Model (MAP), a systematic methodology for the early detection and prevention of cardiovascular disease and broader...`,
+        _detail: `
+        Katlas Technology has developed MAI Health, a clinical system comprising platforms and a portal that enables fast-track, remote monitoring of healthcare. <br><br>
+        MAI Health integrates with medical devices and an AI engine (NEOS) to provide a Proximity Care Model (MAP), a systematic methodology for the early detection and prevention of cardiovascular disease and broader long-term conditions. <br><br>
+        In pilot testing, MAI Health has demonstrated speedy data gathering, management and presentation of information back to the individual in the form of their own personalised health wallet. <br><br>
+        This approach places the patient at the centre of their own healthcare. They can use findings from clinical screenings, socio-economic and lifestyle factors to consult with their GP remotely, who would then review findings, conduct an online diagnosis and prescription. <br><br>
+        The implications for the NHS and other healthcare providers of this innovative patientcentered healthcare are considerable, including optimization of current health resources (human and technological), improvements in the efficiency and effectiveness of patient and public health information management systems, rapid triage services and cost savings. <br><br>
+        AI Health has the potential to revolutionise the way in which healthcare is delivered whilst preventing premature deaths from cardiovascular disease, the prime cause of heart disease, Alzheimer’s and dementia, namely the biggest killers of men and women in the UK. <br><br>
+        The Challenge  <br><br>
+        A huge amount of information is being captured by the NHS each day. Vital patient information sits as data spread across disparate systems across different organisations within the NHS. In the absence of a common platform to search for and then analyse this data, effective patient information management has proved expensive and technically difficult to achieve. The NHS is a reactive service, it is only by being able to integrate these data systems will it be capable of finding the solution to addressing preventable deaths. Combatting cardiovascular disease at an early stage is a big issue for the NHS. The potential for achieving this is within its grasp, as the algorithms for analysing patient data are there. However, centralised systems are not safe, slow to interrogate and require significant human resources to manage them. Combined with insufficient level of securities that one might expect of sensitive information, the result is an ineffective health information service which; <br><br>
+        • increases waiting times for appointments<br>
+         • requires unnecessary travel arrangements <br>
+        • affects patient communication with GPs<br>
+         • can cause disputes between health professionals prescribing and dispensing treatments<br>
+         • is open to security breaches <br>
+         • causes significant anxiety for patients when making appointments<br><br>
+        To date, personalised care with AI integration cannot be delivered because patient data captured remotely from medical devices was unreliable and unsecure. <br><br>
+        Solution<br><br>
+        MAI Health is a medical data service that puts the patient in charge of their own health and wellbeing. MAI Health uses blockchain technology to integrate with all major clinical systems. <br><br>
+        Each individual has all their personal healthcare information safely contained in a digital ‘wallet’, which they control access to. By controlling who sees their data and when, individuals have a greater level of control of their own appointments, treatments and patient history. MAI Health can deliver the reports that the GP needs to know to determine a range of options or treatments. <br><br>
+        My (AI) Health puts patients in control of their personal health 24/7. Importantly, our product puts GDPR consent at the heart of every transaction. Our data management platform maximises privacy, minimises disputes and ensures all resources are tracked and accounted at the point of use. <br><br>
+        Our pilot work to date has sought to show how interaction between GP practices and patients can improve by enabling people to take control of their health issues, safely and securely. <br><br>
+        Services<br><br>
+        MAI Health is different from other health apps in the marketplace because it is powered by high performance KATLAS Blockchain technology that is demonstrably superior than anything else available in the marketplace. <br><br>
+        The COVID19 pandemic has exposed the need for a more efficient clinical system that can monitor patient data remotely. This has presented an opportunity to test the effectiveness of using Katlas Technology’s innovative blockchain network to secure and communicate patient data. The scope of patient data monitored has covered COVID-19 symptoms but as importantly broader long-term condition management in cardiovascular disease (CVD). <br><br>
+        MAI Health can support functionality that just does not exist elsewhere. This concept is proven by integrating patient data to AI and medical devices to show an unbroken chain of data from IoMT to GP. <br><br>
+        Benefits<br><br>
+        For patients, the main benefits are; <br><br>
+        • Improved communication with doctors and other healthcare professionals. <br><br>
+        • Reduces waiting times, wasted trips to the pharmacy and missed appointments.  <br><br>
+        • Responses to medical questions without leaving home. <br><br>
+        • Quicker access to test results. <br><br>
+        • Patients own and control who they share their data with <br><br>
+        • Patient consent in every transaction For the NHS, GPs, clinicians, hospital trusts and CCGs, the main benefits are; <br><br>
+        • Cost savings, with automated services enabling faster delivery of proximity care. <br><br>
+        • Data surety, solving privacy and consent (KYC and GDPR) <br><br>
+        • Saving time and resources spent chasing up referrals, managing disputes and checking identification reduced by 90%. <br><br>
+        • More efficacious patient booking and resource management, reducing workload<br><br>
+        • Safer, compliant integration with medical devices (IoMT)   <br><br>
+        • Minimal disruption, with GPs, hospitals, CCGs, Foundation Trusts, patients, machines, pharmacies, and third-party vendors simply accessing cloud services to join network. <br><br>
+        • Improved interaction between GP practices and patients by encouraging people to take control of their health<br><br>
+        Case Study – Microcirculatory and cardiovascular health prevention<br><br>
+        A sample group of 60 patients was selected based on their propensity to suffer from future cardiovascular problems; <br><br>
+        • 30 males comprising those aged 40-55 (a typically under-reported group which may have hidden symptoms) and those aged 55-70 (higher priority group) with high risk of hypertension. <br><br>
+        • 30 females comprising those aged 50-70 (with possible hidden symptoms) and those aged 40-50 with high stroke risk. <br><br>
+        Each individual was screened, supported by the use of biomedical devices, for clinical, psycho-social and socio-economic measures to determine the health status and likely future prognosis of the individual. Data was analysed using an Artificial Intelligence and Machine Learning system NEOS to provide a comprehensive health profile of each individual. <br><br>
+        Results were achieved in a quicker time than previously possible with a predicted prognosis for each individual with advice on treatment and lifestyle changes. This proactive process of, screening, testing and preventory diagnosis was particularly valuable for one individual who was found to have the early stages of a pulmonary infarction and subsequently successfully treated. <br><br>
+        Early Conclusions from Pilot Work<br><br>
+        The MAI Health system is capable of delivering triage services within a much quicker timescale than currently capable, which in itself saves lives. It can provide proactive care, grade a patient’s condition (high/med/low risk) and recommend treatments or programmes, all supported by apps. <br><br>
+        MAI Health can detect conditions and cases that would have been missed with traditional techniques<br><br>
+        There are significant benefits in cost savings for the NHS and other healthcare providers, taking out the lower value element of the service and offering a different model for healthcare. With this will emerge a new job, that of health coach, who will be able to deploy mobile services and utilise the technology to assist both patients and healthcare professionals. GPs will pay a fee for this service. <br><br>
+        Katlas Technology will provide the technology pipeline with blockchain software on which other applications, such as wearable medical device software will sit to collect data. The blockchain will integrate previously autonomous systems with the IOMT through nodes and link to other systems to co-ordinate service delivery to individuals in their home. <br><br>
+        We are currently at the proof of concept stage and are delivering better results, now seeking need more clinical data for further trials. 
+        `
     }
 
 ]
--- js/projects.js
+++ js/projects.js
@@ -4,14 +4,14 @@ import data from './data.js'
 const blog_card = Util.eleQRY('.blog-card')
 const blogs = Util.eleQRY('.blog-list')
 
-function setInfo(data,id) {
+function setInfo(data, id) {
 
     blog_card.innerHTML = `
 
-    <button style="display:none;" class="minimize"><i class="fas fa-expand-alt"></i></button>
+    <button class="minimize"><i class="fas fa-compress-alt"></i></button>
     <div class="blog-post-info">
         <div class="blog-post-date" style.>
-            <span>${ id>9?id+1:`0${id+1}`}</span>
+            <span>${ id > 9 ? id + 1 : `0${id + 1}`}</span>
         </div>
         <h3 class="blog-title">${data[id]._study}</h3>
         <p class="blog-p">${data[id]._intro}</p>
@@ -24,22 +24,57 @@ function setInfo(data,id) {
     let blog_p = Util.eleQRY('.blog-p')
 
     more_info.addEventListener('click', (e) => {
-        Util.addStyel(blog_card,'blog-card-2dry')
-        Util.addStyel(blogs,'blog-list-2dry')
-        Util.set_style(more_info,{opacity:0})
-        setTimeout(()=> Util.set_style(more_info,{display:'none'}) ,500)
+        Util.addStyel(blog_card, 'blog-card-2dry')
+        Util.addStyel(blogs, 'blog-list-2dry')
+        Util.set_style(more_info, { opacity: 0 })
+        setTimeout(() => Util.set_style(more_info, { display: 'none' }), 500)
         minimize.style.display = 'inline'
         blog_p.innerHTML = data[id]._detail
     })
-    minimize.addEventListener('click',()=>{
-        Util.rmvStyel(blog_card,'blog-card-2dry')
-        Util.rmvStyel(blogs,'blog-list-2dry')
-        Util.set_style(more_info,{display:'block'})
-        setTimeout(()=> Util.set_style(more_info,{opacity:1}) ,10)
+    minimize.addEventListener('click', () => {
+        Util.rmvStyel(blog_card, 'blog-card-2dry')
+        Util.rmvStyel(blogs, 'blog-list-2dry')
+        Util.set_style(more_info, { display: 'block' })
+        setTimeout(() => Util.set_style(more_info, { opacity: 1 }), 10)
         minimize.style.display = 'none'
         blog_p.innerHTML = data[id]._intro
     })
 
 }
 
-setInfo(data,0)
+
+function list_studies() {
+    const list_cont = Util.eleQRY('.blog-list')
+
+    for (let i = 0; i < data.length; i++) {
+        let study = Util.crtEle('div')
+        Util.addStyel(study, 'blog-list-blog')
+        Util.addStyel(study, 'flex-row')
+        study.innerHTML = `
+        
+            <div class="blog-list-date">
+                <p>${ i > 9 ? i + 1 : `0${i + 1}`}</p>
+            </div>
+            <div class="blog-list-blog-content">
+                <h3 class="blog-list-blog-title">${data[i]._study}</h3>
+                
+                <button class="blog-list-expand"><i class="fas fa-angle-double-right"></i></button>
+            </div>
+        
+        `
+    //     < p class="blog-list-blog-p" > Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    //     Quis molestias, earum repudiandae quos, distinctio laudantium,
+    // </p >
+            list_cont.appendChild(study)
+    }
+    let selet_study = Util.eleQRYAll('.blog-list-expand')
+
+    setInfo(data, 0)
+    for (let i = 0; i < selet_study.length; i++) {
+        selet_study[i].addEventListener('click', () => {
+            setInfo(data, i)
+        })
+    }
+}
+
+list_studies()
