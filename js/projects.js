64fdfc52c36d789fde27b808a33ca93ad818f9b1
RiadHossain <riadhossain7464@gmail.com>
2020-07-15 17:56:00 +0600

    New Update

--- js/projects.js
+++ js/projects.js
@@ -1,27 +1,27 @@
 import * as Util from './util.js'
+import data from './data.js'
+
 const blog_card = Util.eleQRY('.blog-card')
 const blogs = Util.eleQRY('.blog-list')
 
-function setInfo() {
+function setInfo(data,id) {
 
     blog_card.innerHTML = `
+
     <button style="display:none;" class="minimize"><i class="fas fa-expand-alt"></i></button>
     <div class="blog-post-info">
-    <div class="blog-post-date">
-        <span>Sunday</span>
-        <span>27 cot,2020</span>
-    </div>
-    <h3 class="blog-title">Blog Title</h3>
-    <p class="blog-p"> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Quis molestias, earum repudiandae quos, distinctio laudantium,
-        sit dolores aliquam exercitationem consectetur a incidunt dolorum
-    </p>
-    <button class="read-more">Read More <i class="fas fa-expand-alt"></i></button>
+        <div class="blog-post-date" style.>
+            <span>${ id>9?id+1:`0${id+1}`}</span>
+        </div>
+        <h3 class="blog-title">${data[id]._study}</h3>
+        <p class="blog-p">${data[id]._intro}</p>
+        <button class="read-more">Read More <i class="fas fa-expand-alt"></i></button>
     </div>
 
 `
     let more_info = Util.eleQRY('.read-more')
     let minimize = Util.eleQRY('.minimize')
+    let blog_p = Util.eleQRY('.blog-p')
 
     more_info.addEventListener('click', (e) => {
         Util.addStyel(blog_card,'blog-card-2dry')
@@ -29,6 +29,7 @@ function setInfo() {
         Util.set_style(more_info,{opacity:0})
         setTimeout(()=> Util.set_style(more_info,{display:'none'}) ,500)
         minimize.style.display = 'inline'
+        blog_p.innerHTML = data[id]._detail
     })
     minimize.addEventListener('click',()=>{
         Util.rmvStyel(blog_card,'blog-card-2dry')
@@ -36,7 +37,9 @@ function setInfo() {
         Util.set_style(more_info,{display:'block'})
         setTimeout(()=> Util.set_style(more_info,{opacity:1}) ,10)
         minimize.style.display = 'none'
+        blog_p.innerHTML = data[id]._intro
     })
 
 }
-setInfo()
+
+setInfo(data,0)
