a36fa73b70f9e686dcf429aa54048cdff12250ec
RiadHossain <riadhossain7464@gmail.com>
2020-07-16 17:32:39 +0600

    New Update

--- js/contactanim.js
+++ js/contactanim.js
@@ -5,6 +5,6 @@ timeline
     .from('.hire p', { y: '100%', opacity: 0, ease: 'elastic' })
     .from('#send_msg',{scale:.8,opacity:0},.7)
     .from('.field',{y:'50%',opacity:0,stagger:.3})
-    .from('.email-hero',{y:'10%',x:'10%', opacity:0,stagger:.3},2)
+    .from('.email-hero',{y:'60%',x:'60%', opacity:0,stagger:.3},2)
     .from('.contact-style',{y:'40%',opacity:0,ease:'bounce'})
     .from('.inline-mail',{rotation:720,duration:.5})
