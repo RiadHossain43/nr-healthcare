1a244ad4729887cfbeb73c485a4ed133c63c0d74
RiadHossain <riadhossain7464@gmail.com>
2020-07-17 22:19:23 +0600

    New Update

--- js/indexanim.js
+++ js/indexanim.js
@@ -1,8 +1,8 @@
 import * as Util from './util.js'
 const timeline = gsap.timeline({ defaults: { duration: .7 } })
      timeline
-         .from('header', { y: '-100%', ease: 'ease' })
-         .from('.title h1', { opacity: 0,webkitFilter:"blur(20px)",ease: 'ease-in-out'  })//ease: 'elastic'
+         .from('header', { y: '-100%', ease: 'ease-in-out' })
+         .from('.title h1', { opacity: 0,webkitFilter:"blur(10px)",ease: 'ease-in-out',scale:1.05 })//ease: 'elastic'
          .from('.title h2', { y: '25%', opacity: 0, duration: .5 })
          .from('.title p', { y: '25%', opacity: 0, duration: .5 })
          .from('.title a', { y: '25%', opacity: 0, duration: .5 })
